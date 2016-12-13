fd2bf5f91347742dce26d5061b91c6ff0247c164
liudongjie <ldjwyyx@163.com>
2016-12-13 17:58:28 +0800

    the first py file

---
+++ worst-python.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+#错误示例
+# def use_mutable_default_param(idx=0, ids=[]):
+#     ids.append(idx)
+#     print(idx)
+#     print(ids)
+
+#正确
+def use_mutable_default_param(idx=0,ids=None):
+	if ids==None:
+		ids=[]
+	ids.append(idx)
+	print idx,ids
+use_mutable_default_param(idx=1)
+use_mutable_default_param(idx=2)
+# list去重
+list1 =[1,2,3,4,4,3,2,1]
+print reduce(lambda x,y: x if y in x else x+[y],[[]]+list1)
